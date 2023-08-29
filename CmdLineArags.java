8f1b25857e89e8785c6d87d0680dab04ba975a13
jayanthmaddukuri <143474272+jayanthmaddukuri@users.noreply.github.com>
2023-08-28 21:07:53 -0700

    Add files via upload

---
+++ CmdLineArags.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public Class cmdlineargs extends Object
+{
+//Object is the superclass of all classes in java
+	public static void main (String args[]) {
+		int x = Integer.parse.Int(args[0]);
+		int y = Integer.parse.Int(args[1]);
+		int z = x + y;
+System.out.println("sum of"+x+" and "+y+" is "+z" ); 
+}
+}+
